890860f261bc55c3264f4e82a3121f5d5c4aa44d
YankkoO <ggomeznido@gmail.com>
2026-04-03 11:26:50 +0200

    Actualizar BaseFileUtil.java

--- gomez_nido_gonzalo_lab10_newsstand/src/uo/mp/lab09/util/file/BaseFileUtil.java
+++ gomez_nido_gonzalo_lab10_newsstand/src/uo/mp/lab09/util/file/BaseFileUtil.java
@@ -1,10 +1,19 @@
-pac
+package uo.mp.lab09.util.file;
 
-public abstract class BaseFileUtil{
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.ArrayList;
+import java.util.List;
 
-
-
-
+/**
+ * A utility class to read/write text lines from/to a text file
+ */
+public abstract class BaseFIleUtil {
 
 
 }
